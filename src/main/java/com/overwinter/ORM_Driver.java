22f199437516cb847357fb2e927635f1d8b3d850
JoelWiegand <16wiegandjr@gmail.com>
2021-09-23 14:51:44 -0500

    update orm_driver

--- src/main/java/com/overwinter/ORM_Driver.java
+++ src/main/java/com/overwinter/ORM_Driver.java
@@ -1,4 +1,5 @@
 package com.overwinter;
+
 import com.overwinter.dummyModels.Test;
 import com.overwinter.util.Configuration;
 
@@ -6,17 +7,16 @@ public class ORM_Driver {
 	public static void main(String[] args) {
 		Configuration cfg = new Configuration();
 		cfg.addAnnotatedClass(Test.class);
-		
+
 		// GET THE ORM UP AND RUNNING
 		OverWinterORM orm = OverWinterORM.getInstance();
-		orm.addTabletoDb(Test.class); // <-- 
+		orm.addTabletoDb(Test.class); // <--
 		Test test = new Test("Kirk", "Hahn");
 
 		Test test2 = new Test("Sam", "Boi");
 		orm.insertObjIntoDB(test);
 		orm.insertObjIntoDB(test2);
 		Test testUpdate = new Test(1, "Thinh", "Pham");
-
 		orm.transaction();
 		orm.insertObjIntoDB(test);
 		orm.updateObjFromDB(testUpdate);
