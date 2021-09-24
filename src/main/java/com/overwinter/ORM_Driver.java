de58475665a4672cbe4658d08d7f7b2c16106afd
Ethan Pham <phamtuongthinh@gmail.com>
2021-09-23 20:21:48 -0400

    Finished the rest of transaction

--- src/main/java/com/overwinter/ORM_Driver.java
+++ src/main/java/com/overwinter/ORM_Driver.java
@@ -1,6 +1,9 @@
 package com.overwinter;
 
+import java.sql.Savepoint;
+
 import com.overwinter.dummyModels.Test;
+import com.overwinter.transaction.Transaction;
 import com.overwinter.util.Configuration;
 
 public class ORM_Driver {
@@ -9,12 +12,25 @@ public class ORM_Driver {
 		cfg.addAnnotatedClass(Test.class);
 		// GET THE ORM UP AND RUNNING
 		OverWinterORM orm = OverWinterORM.getInstance();
-//		orm.addTabletoDb(Test.class); // <-- 
-//		Test test = new Test("Kirk", "Hahn");
-
-		Test test2 = new Test(2,"Skinny", "Boi");
-//		orm.insertObjIntoDB(test);
-//		orm.insertObjIntoDB(test2);
+		orm.addTabletoDb(Test.class); // <-- 
+		Test test = new Test("Kirk", "Hahn");
+		Test test2 = new Test("Thinh", "Pham");
+		Test test3 = new Test("Joel", "Wiegand");
+		
+		orm.beginTransaction();
+		orm.insertObjIntoDB(test); // KIRK inserted
+		orm.setSavePointWithName("testing"); // set save point
+		orm.insertObjIntoDB(test2); // THINH <--- THIS ONE WILL BE INSERTED
+		orm.rollBackWithSpecificSavePoint(Transaction.getSavepoint()); // ROLL
+		orm.insertObjIntoDB(test3); // JOEL inserted
+		orm.commit(); // will have KIRK AND JOEL
+		
+		
+		
+		
+		
+		
+		
 //		Test testUpdate = new Test(1, "Thinh", "Pham");
 
 //		orm.transaction();
@@ -26,9 +42,9 @@ public class ORM_Driver {
 //		orm.commit();
 		
 		// Thinny Boi HERE !!! SHOULD BE V
-		orm.addAllFromDBToCache(Test.class); // this will be call the first time so it makes senes but not the 2nd time
-		orm.updateObjFromDB(test2);
-		orm.putObjectInCache(test2); // THIS WILL BE SKINN BOI!!!!
+//		orm.addAllFromDBToCache(Test.class); // this will be call the first time so it makes senes but not the 2nd time
+//		orm.updateObjFromDB(test2);
+//		orm.putObjectInCache(test2); // THIS WILL BE SKINN BOI!!!!
 		// this SHOULD return the whole cache including update version of id2 = skinny boi
 		
 		// This part was repeated but we will delete later
@@ -46,5 +62,9 @@ public class ORM_Driver {
 //		orm.deleteObjFromDB(test);
 //		orm.commit();
 //		orm.addAllFromDBToCache(Test.class, orm);
+		
+		
+		
+		
 	}
 }
